2c3dfdf9200c812795918daef593fb527c794e52
MrKurilin <kurilin-anton@mail.ru>
2023-09-23 10:30:07 +0300

    rename App

--- app/src/main/java/ru/mrkurilin/mrKurilinCore/App.kt
+++ app/src/main/java/ru/mrkurilin/mrKurilinCore/App.kt
@@ -1,13 +1,13 @@
 package ru.mrkurilin.mrKurilinCore
 
 import android.app.Application
-import ru.mrkurilin.mrKurilinCore.di.api.SubComponentsProvider
-import ru.mrkurilin.mrKurilinCore.di.api.SubComponentsProviderProvider
 import ru.mrkurilin.mrKurilinCore.di.AppComponent
 import ru.mrkurilin.mrKurilinCore.di.DaggerAppComponent
 import ru.mrkurilin.mrKurilinCore.di.SubComponentsProviderImpl
+import ru.mrkurilin.mrKurilinCore.di.api.SubComponentsProvider
+import ru.mrkurilin.mrKurilinCore.di.api.SubComponentsProviderProvider
 
-class mrKurilinCore : Application(), SubComponentsProviderProvider {
+class App : Application(), SubComponentsProviderProvider {
 
     private lateinit var appComponent: AppComponent
     private lateinit var subComponentsProviderImpl: SubComponentsProvider
